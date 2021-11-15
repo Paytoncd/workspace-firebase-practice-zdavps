30d506e6debecc0136481638ceecb285dfa645e9
Paytoncd <paytoncd@usca.edu>
2021-11-15 13:16:54 -0500

    3 files modified

--- js/Signup.js
+++ js/Signup.js
@@ -1,12 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM",
-  authDomain: "grocerylist-91956.firebaseapp.com",
-  databaseURL: "https://grocerylist-91956.firebaseio.com",
-  projectId: "grocerylist-91956",
-  storageBucket: "grocerylist-91956.appspot.com",
-  messagingSenderId: "813812426276",
-  appId: "1:813812426276:web:93e5897af12892ff78dab1",
-  measurementId: "G-VZ83BTR72T"
+  apiKey: "AIzaSyDkbC5Y8wMKr3kA728t9pStSn-jfH2k05A",
+    authDomain: "csci225-fd266.firebaseapp.com",
+    projectId: "csci225-fd266",
+    storageBucket: "csci225-fd266.appspot.com",
+    messagingSenderId: "547337845521",
+    appId: "1:547337845521:web:ea2ac0d8b32a804ffb2ab0",
+    measurementId: "G-JJWB6RWW5V"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -16,7 +15,8 @@ $("#signup-form").submit(function(e) {
   e.preventDefault();
   //get the username(email) and password from the form
   // change the following code
-  var email = "yilianz4@gmail.com";
+  var email = $("#signup-form input[name='username']").val();
+  console.log('email you typed is' + email);
   var password = "ddsgagafda";
 
   // create a user with email address and password
@@ -36,4 +36,33 @@ $("#signup-form").submit(function(e) {
       console.log(error.code);
       console.log(errorMessage);
     });
+});
+
+
+//the other chocie use google account 
+$('#google').click(function(){
+console.log("Click google log in method");
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
 });
